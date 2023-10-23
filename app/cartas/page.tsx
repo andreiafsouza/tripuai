8404fde8ceb386a83793309d5ab100a18db0a9e5
fsouza.andreia <fsouza.andreia@gmail.com>
2023-10-23 18:20:54 +0100

    feat: separate card points in groups and assign to each city

--- app/cartas/page.tsx
+++ app/cartas/page.tsx
@@ -1,11 +1,8 @@
-"use client";
-import React, { useState, ChangeEvent } from "react";
+import React from "react";
 import citiesData from "../../cities.json";
-import { CityCard } from "@/components/CityCard/CityCard";
-import { CityCardDisplay } from "@/components/CityCardDisplay/CityCardDisplay";
-import SelectCityButton from "@/components/SelectCityButton/SelectCityButton";
 /* style */
 import styles from "./styles.module.css";
+import CityCardList from "@/components/CityCardList/CityCardList";
 
 export type CityProps = {
   id: number;
@@ -19,72 +16,10 @@ export type CityProps = {
 
 const CitiesCardListPage = () => {
   const cities: CityProps[] = citiesData;
-  const [selectedCity, setSelectedCIty] = useState(cities[0].nome);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [suggestions, setSuggestions] = useState<CityProps[]>([]);
-
-  const handleChangeCityCard = (name: string) => {
-    const cardName: CityProps[] = cities.filter((city) => name === city.nome);
-    const selectedName = cardName[0].nome;
-    setSelectedCIty(selectedName);
-  };
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    setSearchTerm(inputValue);
-
-    const filteredCities = cities.filter(
-      (city) =>
-        city.nome.toLowerCase().includes(inputValue.toLowerCase()) &&
-        /^[a-zA-Z]+$/.test(inputValue) // Check if input contains only letters
-    );
-
-    setSuggestions(filteredCities);
-  };
 
   return (
     <main className={styles.container}>
-      <section className={styles.card}>
-        <div className={styles.search}>
-          <input
-            className={styles.input}
-            type="text"
-            name="search"
-            value={searchTerm}
-            onChange={handleInputChange}
-          />
-
-          {suggestions.length > 0 && (
-            <div className={styles.suggestions}>
-              <ul className={styles.suggestionsList}>
-                {suggestions.map((city) => (
-                  <li
-                    className={styles.suggestionsItem}
-                    key={city.id}
-                    onClick={() => handleChangeCityCard(city.nome)}
-                  >
-                    {city.nome}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-        <div className={styles.selected}>
-          <CityCardDisplay title={selectedCity} />
-        </div>
-      </section>
-      <section className={styles.list}>
-        {cities.map((city) => (
-          <SelectCityButton
-            key={city.id}
-            title={city.nome}
-            selectCity={handleChangeCityCard}
-          >
-            <CityCard title={city.nome} />
-          </SelectCityButton>
-        ))}
-      </section>
+      <CityCardList cities={cities} />
     </main>
   );
 };
---
+++ app/components/CityCardList/CityCardList.tsx
@@ -0,0 +1,121 @@
+"use client";
+import React, { useState, ChangeEvent, useEffect } from "react";
+import { CityCard } from "@/components/CityCard/CityCard";
+import { CityCardDisplay } from "@/components/CityCardDisplay/CityCardDisplay";
+import SelectCityButton from "@/components/SelectCityButton/SelectCityButton";
+import { CityProps } from "cartas/page";
+/* style */
+import styles from "./styles.module.css";
+
+const CityCardList = ({ cities }: { cities: CityProps[] }) => {
+  const [selectedCity, setSelectedCIty] = useState(cities[0].nome);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState<CityProps[]>([]);
+  const [cityCards, setCityCards] = useState<CityProps[]>([]);
+
+  const handleChangeCityCard = (name: string) => {
+    const cardName: CityProps[] = cities.filter((city) => name === city.nome);
+    const selectedName = cardName[0].nome;
+    setSelectedCIty(selectedName);
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setSearchTerm(inputValue);
+
+    const filteredCities = cityCards.filter(
+      (city) =>
+        city.nome.toLowerCase().includes(inputValue.toLowerCase()) &&
+        /^[a-zA-Z]+$/.test(inputValue) // Check if input contains only letters
+    );
+
+    setSuggestions(filteredCities);
+  };
+
+  const assignGroups = (cities: CityProps[]) => {
+    const properties: (keyof CityProps)[] = ["top", "right", "left", "bottom"];
+    const groupSize = [87, 85, 85, 85, 85, 85, 85, 85, 85, 86];
+
+    const groupedCities: CityProps[] = cities.map((city) => ({ ...city }));
+
+    properties.forEach((property, index) => {
+      groupedCities.sort(
+        (a, b) => (a[property] as number) - (b[property] as number)
+      );
+
+      let group = 1;
+      let groupCount = 0;
+
+      for (const city of groupedCities) {
+        (city[property] as number) = group;
+
+        if (groupCount === groupSize[index]) {
+          group++;
+          groupCount = 0;
+        } else {
+          groupCount++;
+        }
+      }
+    });
+
+    return groupedCities;
+  };
+
+  useEffect(() => {
+    if (cities) {
+      const cityCardList: CityProps[] = assignGroups(cities).sort((a, b) => {
+        return a.nome.localeCompare(b.nome);
+      });
+
+      setCityCards(cityCardList);
+    }
+  }, []);
+
+  return (
+    <>
+      <section className={styles.card}>
+        <div className={styles.search}>
+          <input
+            className={styles.input}
+            type="text"
+            name="search"
+            value={searchTerm}
+            onChange={handleInputChange}
+          />
+
+          {suggestions.length > 0 && (
+            <div className={styles.suggestions}>
+              <ul className={styles.suggestionsList}>
+                {suggestions.map((city) => (
+                  <li
+                    className={styles.suggestionsItem}
+                    key={city.id}
+                    onClick={() => handleChangeCityCard(city.nome)}
+                  >
+                    {city.nome}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className={styles.selected}>
+          <CityCardDisplay title={selectedCity} />
+        </div>
+      </section>
+      <section className={styles.list}>
+        {cityCards.map((city) => (
+          <SelectCityButton
+            key={city.id}
+            title={city.nome}
+            selectCity={handleChangeCityCard}
+          >
+            <CityCard title={city.nome} />
+          </SelectCityButton>
+        ))}
+      </section>
+    </>
+  );
+};
+
+export default CityCardList;
