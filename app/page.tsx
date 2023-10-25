e8e07f3c35db1328285ce33aa437dbde10b38c8c
fsouza.andreia <fsouza.andreia@gmail.com>
2023-10-25 01:27:25 +0100

    style: sexperimenting with bg mesh and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
+import tripUai from "../public/tripuai-logo.svg";
+import style from "./styles.module.css";
 
 export default function Home() {
   return (
     <main>
+      <Image
+        src={tripUai}
+        alt=""
+        //width={558}
+        //height={152}
+        // blurDataURL="data:..." automatically provided
+        // placeholder="blur" // Optional blur-up while loading
+      />
       <h1>HELLO WORLD</h1>
       <Link href="/cartas">Cartas</Link>
     </main>
