c401fc60aa633c5034099641a94828cd78d49321
fsouza.andreia <fsouza.andreia@gmail.com>
2023-10-25 20:04:10 +0100

    style: adjust logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
 import Link from "next/link";
 import Image from "next/image";
-import tripUai from "../public/tripuai-logo.svg";
-import style from "./styles.module.css";
+import logoBg from "../public/logo-bg.svg";
+import teste from "../public/title-teste-ii.svg";
+import styles from "./styles.module.css";
 
 export default function Home() {
   return (
     <main>
-      <Image
-        src={tripUai}
-        alt=""
-        //width={558}
-        //height={152}
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      />
+      <div className={styles.wrapper}>
+        <Image
+          className={styles.logo}
+          src={teste}
+          alt=""
+          //width={558}
+          //height={152}
+          // blurDataURL="data:..." automatically provided
+          // placeholder="blur" // Optional blur-up while loading
+        />
+        <Image
+          className={styles.logoBg}
+          src={logoBg}
+          alt=""
+          //width={558}
+          //height={152}
+          // blurDataURL="data:..." automatically provided
+          // placeholder="blur" // Optional blur-up while loading
+        />
+      </div>
+
       <h1>HELLO WORLD</h1>
       <Link href="/cartas">Cartas</Link>
     </main>
